4f3df1c35151b9a0ce84542179fb7d23e74091d2
DESKTOP-KL0E6A7\hjt-zy <315862477@qq.com>
2021-04-27 08:22:54 +0800

    feat: 引入eslint husky

---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>主页</h2>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Home',
+    setup() {}
+});
+</script>
+
+<style></style>
